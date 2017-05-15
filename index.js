578c2fff4e231d897ef3af63e493332433290dba
Your Full Name <youremailaddress@example.com>
2017-05-15 15:39:11 -0400

    loop with no color

--- index.js
+++ index.js
@@ -5,30 +5,42 @@ const handleSubmit = (ev) => {
     const form = ev.target
     const details = document.querySelector('.details')
 
-    const personName = form.personName.value
-    const hairColor = form.hairColor.value
-    const age = form.age.value
-    const birthplace = form.birthplace.value
+    // const personName = form.personName.value
+    // const hairColor = form.hairColor.value
+    // const age = form.age.value
+    // const birthplace = form.birthplace.value
 
-    const colorDiv = `
-        <div style="height: 50px; width: 50px; background-color: ${hairColor}"></div>
-    `
+    // const colorDiv = `
+    //     <div style="height: 50px; width: 50px; background-color: ${hairColor}"></div>
+    // `
+    const ul = document.createElement('ul')
+    ul.textContent = 'Submission'
+    details.appendChild(ul)
 
-    const em = document.createElement('ul')
-    em.textContent = 'Submission'
-    const name = document.createElement('li')
-    name.textContent = personName
-    const color = document.createElement('li')
-    color.textContent = hairColor
-    const num = document.createElement('li')
-    num.textContent = age
-    const place = document.createElement('li')
-    place.textContent = birthplace
-    em.appendChild(name)
-    em.appendChild(color)
-    em.appendChild(num)
-    em.appendChild(place)
-    details.appendChild(em)
+    const labels = document.getElementsByTagName('label')
+
+    for(var i = 0; i < form.length - 1 ; i++) {
+        const list = document.createElement('li')
+        list.textContent = labels[i].innerText + ": " + form[i].value
+        ul.appendChild(list)        
+    }
+
+    
+    // const em = document.createElement('ul')
+    // em.textContent = 'Submission'
+    // const name = document.createElement('li')
+    // name.textContent = personName
+    // const color = document.createElement('li')
+    // color.textContent = hairColor
+    // const num = document.createElement('li')
+    // num.textContent = age
+    // const place = document.createElement('li')
+    // place.textContent = birthplace
+    // em.appendChild(name)
+    // em.appendChild(color)
+    // em.appendChild(num)
+    // em.appendChild(place)
+    // details.appendChild(em)
 
 }
 
