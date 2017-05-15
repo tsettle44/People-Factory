f375fc55c5085f3d77a3360c01c7a1b3de398fb9
Your Full Name <youremailaddress@example.com>
2017-05-15 11:59:06 -0400

    readme

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const personForm = document.querySelector('form')
 
-personForm.onsubmit = (ev) => {
+personForm.onsubmit = 
+
+const handSubmit = (ev) => {
     ev.preventDefault()
     const form = ev.target
-    const details = document.querySelector('.details')
+    const details = document.querySelector(`.details`)
 
-    const personName = form.personName
-    console.log(personName)
+    const personName = form.personName.value
+    const hairColor = form.personName.value
+    const age = form.personName.value
+    const birthplace = form.personName.value
+
+    const colorDiv = `
+        <div style="height: 50px; width: 50px; background-color: ${hairColor}"></div>
+    `
 
     // details.innerHTML = `<em>${personName}</em>`
 
     // const em = document.createElement('em')
     // em.textContent = personName
     // details.appendChild(em)
+
+    details.innerHTML = `
+        <ul>
+            <li>Name: ${personName}</li>
+            <li>Hair Color: ${hairColor}</li>
+            <li>Age: ${age}</li>
+            <li>Birthplace: ${birthplace}</li>
+        </ul>
+    `
 }
+
+person.addEventListener('submit', handleSubmit)
