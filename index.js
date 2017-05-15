9d16373f7fda7c4c4438ac7c30bca433b372fc7a
Your Full Name <youremailaddress@example.com>
2017-05-15 11:32:21 -0400

    add stylesheets

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const personForm = document.querySelector('form')
 personForm.onsubmit = (ev) => {
     ev.preventDefault()
     const form = ev.target
-    const details = document.querySelector('div.details')
+    const details = document.querySelector('.details')
 
-    const personName = form.personName.value
+    const personName = form.personName
+    console.log(personName)
 
-    details.textContent = personName
+    // details.innerHTML = `<em>${personName}</em>`
+
+    // const em = document.createElement('em')
+    // em.textContent = personName
+    // details.appendChild(em)
 }
