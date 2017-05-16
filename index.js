8835e65a8dd48564084fe054aa741baa0d2f5403
Your Full Name <youremailaddress@example.com>
2017-05-15 22:24:17 -0400

    border and final js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const handleSubmit = (ev) => {
 
     for(var i = 0; i < form.length - 1 ; i++) {
         if(i == 1) {
-            const colorDiv = `<div style="height: 50px; width: 50px; background-color: ${form[i].value}"></div>`
+            const colorDiv = `<div style="height: 20px; width: 150px; background-color: ${form[i].value}"></div>`
             const list = document.createElement('li')
             list.innerHTML = labels[i].innerText + ": " + colorDiv
             ul.appendChild(list)
@@ -50,5 +50,6 @@ const handleSubmit = (ev) => {
     // details.appendChild(em)
 
 }
+
 
 personForm.addEventListener('submit', handleSubmit)
